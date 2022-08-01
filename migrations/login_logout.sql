38ca39d388521b44787032b245d9f2983b9b0b99
Kamtay5151 <107449285+Kamtay5151@users.noreply.github.com>
2022-07-31 21:34:29 -0500

    Update login_logout.sql

--- migrations/login_logout.sql
+++ migrations/login_logout.sql
@@ -1,30 +1,31 @@
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `user` (
 `user_id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--
-INSERT INTO `users` (`user_id`, `username`, `password`, `email`) VALUES
+
+INSERT INTO `user` (`user_id`, `username`, `password`, `email`) VALUES
 (2, 'admin', 'admin', 'admin'),
 (3, 'user', 'user', 'user');
 
 --
-ALTER TABLE `users`
+ALTER TABLE `user`
  ADD PRIMARY KEY (`user_id`);
 
 --
-ALTER TABLE `users`
+ALTER TABLE `user`
 MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 
 // login user
-router.post('/login', (req, res)=>{
-    if(req.body.Username == credential.Username && req.body.password == credential.password){
-        req.session.email = req.body.email;
-        res.redirect('/route/patientrecords.dashboard');
+
+if(user == usernames && password && email){
+              res.redirect('/route/patientrecords.dashboard');
         //res.end("Login Successful...!");
-    }else{
+    }
+    
+    else{
         res.end("Invalid Username")
     }
 });
