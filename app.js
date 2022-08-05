7f6439a05ffc2f4a148e51311d4fb1bcfa4b66dd
Cory <cory@thelodys.com>
2022-08-04 19:09:21 -0500

    add ability to sort appointments

--- app.js
+++ app.js
@@ -89,6 +89,7 @@ app.post("/deleteappt", async (req, res) => {
 
 app.get("/apptcal", async (req, res) => {
   const db = await dbPromise;
+  const sort = "time";
   const appointments = await db.all(`
   select p.patient_givenname, p.patient_surname, d.doc_givenname, d.doc_surname, r.room_number, a.time, a.appt_id 
   from appointments a
@@ -97,7 +98,21 @@ app.get("/apptcal", async (req, res) => {
   INNER JOIN rooms r on r.room_id = a.room_id
   ORDER BY a.time
   `);
-  res.render("apptcal", {appointments});
+  res.render("apptcal", {appointments, sort});
+});
+
+app.post("/apptcal", async (req, res) => {
+  const db = await dbPromise;
+  const { sort } = req.body;
+  const appointments = await db.all(`
+  select p.patient_givenname, p.patient_surname, p.patient_id, d.doc_givenname, d.doc_surname, d.doc_id, r.room_number, r.room_id, a.time, a.appt_id 
+  from appointments a
+  INNER JOIN patients p on p.patient_id = a.patient_id
+  INNER JOIN doctors d on d.doc_id = a.doc_id
+  INNER JOIN rooms r on r.room_id = a.room_id
+  ORDER BY a.time
+  `);
+  res.render("apptcal", {appointments, sort});
 });
 
 app.get("/recreq", async (req, res) => {
